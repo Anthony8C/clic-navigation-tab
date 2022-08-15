5d22f2a1b8bd3cdf72312ad8bf761fa8571bb683
Anthony8C <acardenas@itsqmet.edu.ec>
2022-08-14 22:05:52 -0500

    Aplicar boton de cerrar sesion
    Cerrar Sesión

--- app/screens/Main/User.js
+++ app/screens/Main/User.js
@@ -1,15 +1,31 @@
-import { View, Text, StyleSheet,Pressable } from 'react-native'
+import { View, Text, StyleSheet, Pressable, Alert } from 'react-native'
 import React from 'react'
+import { getAuth, signOut } from "firebase/auth";
 
 export default function User() {
+
+  const cerrar = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      Alert.alert('Cerrando Sesión');
+    }).catch((error) => {
+      // An error happened.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Alert.alert("Error: " + errorCode + "Message: " + errorMessage);
+    });
+  }
+
+
   return (
     <View style={styles.container}>
-       <Pressable
-        /* onPress={} */
+      <Pressable
+        onPress={cerrar}
         style={styles.button}
-       >
+      >
         <Text style={styles.textButton}>Cerrar Sesión</Text>
-       </Pressable>
+      </Pressable>
     </View>
   )
 }
@@ -22,19 +38,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   button: {
-      marginTop: 20,
-      marginBottom: 30,
-      padding: 10,
-      backgroundColor: "#02CCFF",
-      borderRadius: 7,
-      width: 300,
-      height: 50,
-      alignItems: "center",
-      justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 30,
+    padding: 10,
+    backgroundColor: "#02CCFF",
+    borderRadius: 7,
+    width: 300,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
   },
   textButton: {
-      color: "#fff",
-      fontSize: 18,
-      fontWeight: "bold",
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   }
 });
